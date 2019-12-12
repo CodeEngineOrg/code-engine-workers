09142e08453587609cf7c58fe6b518e22099c471
James Messinger <james.messinger@gmail.com>
2019-12-12 04:40:19 -0600

    Export the `WorkerPool` as both a default and named export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export { WorkerPool } from "./main-thread/worker-pool";
+import { WorkerPool } from "./main-thread/worker-pool";
+
+export { WorkerPool };
+
+// Export `WorkerPool` as the default export
+// tslint:disable: no-default-export
+export default WorkerPool;
--- test/specs/exports.spec.js
+++ test/specs/exports.spec.js
@@ -1,22 +1,31 @@
 "use strict";
 
 const commonJSExport = require("../../");
-const { default: defaultExport, WorkerPool } = require("../../");
+const { default: defaultExport } = require("../../");
+const { WorkerPool: namedExport } = require("../../");
 const { expect } = require("chai");
 
 describe("@code-engine/workers package exports", () => {
 
-  it("should not have a default ESM export", () => {
-    expect(defaultExport).to.be.equal(undefined);
+  it("should export the module as the default CommonJS export", () => {
+    expect(commonJSExport).to.be.an("object");
+    expect(commonJSExport.default).to.equal(defaultExport);
+    expect(commonJSExport.WorkerPool).to.equal(namedExport);
+  });
+
+  it("should export the WorkerPool class as the default ESM export", () => {
+    expect(defaultExport).to.be.a("function");
+    expect(defaultExport.name).to.equal("WorkerPool");
   });
 
   it("should export the WorkerPool class as a named export", () => {
-    expect(WorkerPool).to.be.a("function");
-    expect(WorkerPool.name).to.equal("WorkerPool");
+    expect(namedExport).to.be.a("function");
+    expect(namedExport.name).to.equal("WorkerPool");
   });
 
   it("should not export anything else", () => {
     expect(commonJSExport).to.have.keys(
+      "default",
       "WorkerPool",
     );
   });
